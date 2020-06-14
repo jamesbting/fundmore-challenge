59a96aa0301d9f70b71280cde52a4d8b06642793
James <tingjames2000@outlook.com>
2020-06-13 18:10:40 -0700

    Cleaned up the code

--- src/Components/Result/ResultItem/ResultItem.jsx
+++ src/Components/Result/ResultItem/ResultItem.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-import CardActionArea from "@material-ui/core/CardActionArea";
 import CardMedia from "@material-ui/core/CardMedia";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import IconButton from "@material-ui/core/IconButton";
@@ -30,11 +28,6 @@ export default class ResultItem extends React.Component {
     }
     return null;
   }
-  // componentWillReceiveProps(newProps) {
-  //   if (newProps.hero.toString() !== this.props.hero.toString()) {
-  //     this.setState({ hero: newProps.hero });
-  //   }
-  // }
 
   //handle when expanded
   handleExpandClick = () => {
--- src/Components/Team/Team.jsx
+++ src/Components/Team/Team.jsx
@@ -93,18 +93,4 @@ export default class Team extends React.Component {
     }
     return newAverages;
   }
-
-  static areEqualShallow(a, b) {
-    for (var key in a) {
-      if (!(key in b) || a[key] !== b[key]) {
-        return false;
-      }
-    }
-    for (var key in b) {
-      if (!(key in a) || a[key] !== b[key]) {
-        return false;
-      }
-    }
-    return true;
-  }
 }
