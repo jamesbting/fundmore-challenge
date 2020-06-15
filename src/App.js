4aa36e0ad3d97daf28ed68fd90dd68a8f1e0f267
James <tingjames2000@outlook.com>
2020-06-14 18:12:26 -0700

    Updated tab to have a real title

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
 import SuperHeroApp from "./Components/SuperHeroApp";
+import { Helmet } from "react-helmet";
 
+const TITLE = "Superhero Team Builder";
 function App() {
-  return <SuperHeroApp></SuperHeroApp>;
+  return (
+    <div className="App">
+      {/* Modify the head element to ensure the tab has the correct title */}
+      <Helmet>
+        {" "}
+        <title>{TITLE}</title>
+      </Helmet>
+      <SuperHeroApp></SuperHeroApp>
+    </div>
+  );
 }
 
 export default App;
