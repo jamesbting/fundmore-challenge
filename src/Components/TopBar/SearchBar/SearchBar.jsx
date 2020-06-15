ef8f7b7f95e9b52ccf262986a1bb1185267e1470
James <tingjames2000@outlook.com>
2020-06-15 11:49:43 -0700

    Added a SearchBar component

---
+++ src/Components/TopBar/SearchBar/SearchBar.jsx
@@ -0,0 +1,83 @@
+//functional component that returns a search bar, that can be used to search for superheros in the API
+//it takes as props an changeQueryHandler, that allows this class to modify the state of its parent (or grandparent) components
+//and sets the state of the parent component to have the new query, so the parent component can do as it pleases with
+
+import React from "react";
+import { fade, makeStyles } from "@material-ui/core/styles";
+import InputBase from "@material-ui/core/InputBase";
+import SearchIcon from "@material-ui/icons/Search";
+
+//some styles to make it pretty
+const useStyles = makeStyles((theme) => ({
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(3),
+      width: "auto",
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
+    },
+  },
+}));
+
+//precondition: props.changeQueryHandler must be a function that modifies the state of the parent component from which it originates, props must be an object
+//postcondition: this function returns a search bar component that will accept queries and pass them to the parent component by updating the state of the parent component
+export default function SearchBar(props) {
+  const classes = useStyles();
+
+  //take the event and the handler that was passed down as a prop, and change the query of the SuperHeroApp class
+  const onChangeQueryHandler = (event) => {
+    const changeQuery = props.changeQueryHandler;
+    changeQuery(event);
+  };
+
+  return (
+    <>
+      <div className={classes.search}>
+        <div className={classes.searchIcon}>
+          <SearchIcon />
+        </div>
+        <InputBase
+          placeholder="Search for a superhero…"
+          classes={{
+            root: classes.inputRoot,
+            input: classes.inputInput,
+          }}
+          onKeyPress={(event) => onChangeQueryHandler(event)}
+          inputProps={{
+            "aria-label": "search",
+          }}
+        />
+      </div>
+      <div className={classes.grow} />
+    </>
+  );
+}
--- src/Components/TopBar/TopBar.jsx
+++ src/Components/TopBar/TopBar.jsx
@@ -4,13 +4,12 @@
 //new query should be submitted to the API or not (when the user presses the enter key)
 
 import React from "react";
-import { fade, makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import InputBase from "@material-ui/core/InputBase";
-import SearchIcon from "@material-ui/icons/Search";
 import Button from "@material-ui/core/Button";
+import SearchBar from "./SearchBar/SearchBar";
 
 //styles for the bar
 const useStyles = makeStyles((theme) => ({
@@ -24,52 +23,9 @@ const useStyles = makeStyles((theme) => ({
       display: "block",
     },
   },
-  search: {
-    position: "relative",
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    "&:hover": {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
-    },
-    marginRight: theme.spacing(2),
-    marginLeft: 0,
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      marginLeft: theme.spacing(3),
-      width: "auto",
-    },
-  },
-  searchIcon: {
-    padding: theme.spacing(0, 2),
-    height: "100%",
-    position: "absolute",
-    pointerEvents: "none",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  inputRoot: {
-    color: "inherit",
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("md")]: {
-      width: "20ch",
-    },
-  },
 }));
 
 export default function TopBar(props) {
-  //take the event and the handler that was passed down as a prop
-  const onChangeQueryHandler = (event) => {
-    const changeQuery = props.changeQueryHandler;
-    changeQuery(event);
-  };
-
   const changeViewHandler = () => {
     const changeView = props.changeViewHandler;
     changeView();
@@ -85,21 +41,7 @@ export default function TopBar(props) {
           <Typography className={classes.title} variant="h6" noWrap>
             Superhero Team Builder
           </Typography>
-          <div className={classes.search}>
-            <div className={classes.searchIcon}>
-              <SearchIcon />
-            </div>
-            <InputBase
-              placeholder="Search for a superhero…"
-              classes={{
-                root: classes.inputRoot,
-                input: classes.inputInput,
-              }}
-              onKeyPress={(event) => onChangeQueryHandler(event)}
-              inputProps={{ "aria-label": "search" }}
-            />
-          </div>
-          <div className={classes.grow} />
+          <SearchBar changeQueryHandler={props.changeQueryHandler} />
           <Button onClick={() => changeViewHandler()} variant="contained">
             {props.message}
           </Button>
