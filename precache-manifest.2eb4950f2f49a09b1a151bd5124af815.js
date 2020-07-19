abcec871a7fb2c8de2a84a795fe209245a0f61a6
James <tingjames2000@outlook.com>
2020-07-19 15:14:22 -0700

    Updates

---
+++ precache-manifest.2eb4950f2f49a09b1a151bd5124af815.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9272d9d3abb5740ee7266ea8fdb94220",
+    "url": "/superhero-webapp/index.html"
+  },
+  {
+    "revision": "377aff9cba3e646f0181",
+    "url": "/superhero-webapp/static/css/main.19728fb2.chunk.css"
+  },
+  {
+    "revision": "0e1f62be5da16d6f8019",
+    "url": "/superhero-webapp/static/js/2.ccdee460.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/superhero-webapp/static/js/2.ccdee460.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "377aff9cba3e646f0181",
+    "url": "/superhero-webapp/static/js/main.5c21c57f.chunk.js"
+  },
+  {
+    "revision": "d1be95ba446fb0937ec5",
+    "url": "/superhero-webapp/static/js/runtime-main.b7dbe031.js"
+  }
+]);
