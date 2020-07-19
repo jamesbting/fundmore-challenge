d605b91c0c6a3f52172d3e89e70b8b5c2981c605
James <tingjames2000@outlook.com>
2020-07-19 15:23:23 -0700

    Updates

---
+++ precache-manifest.4dff9e71f0db74b827f845816d50338f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13e2c3607334f1fda6b9196fd3f1dd39",
+    "url": "/superhero-team-builder/index.html"
+  },
+  {
+    "revision": "9208d78ae0357f2a793f",
+    "url": "/superhero-team-builder/static/css/main.19728fb2.chunk.css"
+  },
+  {
+    "revision": "74f8e4bc70ab709e6d3f",
+    "url": "/superhero-team-builder/static/js/2.e2406226.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/superhero-team-builder/static/js/2.e2406226.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9208d78ae0357f2a793f",
+    "url": "/superhero-team-builder/static/js/main.22d32d3b.chunk.js"
+  },
+  {
+    "revision": "ab2a05bc204c7e4ba222",
+    "url": "/superhero-team-builder/static/js/runtime-main.bff28913.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/superhero-webapp/precache-manifest.2eb4950f2f49a09b1a151bd5124af815.js"
+  "/superhero-team-builder/precache-manifest.4dff9e71f0db74b827f845816d50338f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/superhero-webapp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/superhero-team-builder/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
